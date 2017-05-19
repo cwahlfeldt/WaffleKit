e126101149014ce414b9e401af35d38cd262323f
cwahlfeldt <chriswahlfeldt@gmail.com>
2017-05-19 15:26:22 -0500

    minor style updates

--- app/client/containers/App/App.js
+++ app/client/containers/App/App.js
@@ -9,6 +9,12 @@ export default class App extends Component {
   render() {
     return (
       <main className={'app-container'}>
+        {/* fixed border elements */}
+        <div className="fixed-border left"></div>
+        <div className="fixed-border right"></div>
+        <div className="fixed-border top"></div>
+        <div className="fixed-border bottom"></div>
+
         <Navbar />
         <Jumbo />
       </main>
